3c982cf2675d30d33d77647dd6e7f5351ab3bfd0
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-02-13 13:04:46 +0100

    getBaseUrl

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@
 export { menuRegistry, getMenu, getMenuNames, findMenuItemById, useMenu, debugMenuRegistry, validateMenus, getMenuTree, searchMenusByTitle, exportMenusAsJSON, getMenuStats } from '@menu/index'
 export type { MenuItemConfig, PackageMenuConfig, MenuBuilder } from '@menu/index'
 
+// Utils
+export { getBaseUrl } from './lib/utils'
+
 // Components
 export { default as AdminMenu } from './components/AdminMenu.vue'
 export { default as ProfileMenu } from './components/layout/ProfileMenu.vue'
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import config from '@config'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+// Visszaadja az alkalmazás alap URL-jét (SERVER_URL)
+export function getBaseUrl(): string {
+  return config.SERVER_URL
 }
