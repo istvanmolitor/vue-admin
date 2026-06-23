12f51da45a60c4b045df959c62587c9357cd912e
Molitor István <istvanmolitor@gmail.com>
2026-06-23 23:39:09 +0200

    checkbox bug

--- components/ui/Checkbox.vue
+++ components/ui/Checkbox.vue
@@ -5,15 +5,15 @@ import type { HTMLAttributes } from 'vue'
 import { cn } from '@admin/lib/utils'
 
 const props = defineProps<{
-  checked?: boolean
+  modelValue?: boolean
   class?: HTMLAttributes['class']
 }>()
 
 const emits = defineEmits<{
-  (e: 'update:checked', payload: boolean): void
+  (e: 'update:modelValue', payload: boolean): void
 }>()
 
-const checked = useVModel(props, 'checked', emits, {
+const checked = useVModel(props, 'modelValue', emits, {
   passive: true,
 })
 </script>
@@ -23,6 +23,7 @@ const checked = useVModel(props, 'checked', emits, {
     type="button"
     role="checkbox"
     :aria-checked="checked"
+    :data-state="checked ? 'checked' : 'unchecked'"
     :class="
       cn(
         'peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
