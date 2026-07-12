7d7b9b79f00abc07d258ae2b5b196550cfc6bc25
Molitor István <istvanmolitor@gmail.com>
2026-07-13 00:42:02 +0200

    attrs

--- components/ui/InputField.vue
+++ components/ui/InputField.vue
@@ -22,11 +22,13 @@ const emits = defineEmits<{
   (e: 'update:modelValue', payload: string | number): void
 }>()
 
+defineOptions({ inheritAttrs: false })
+
 const modelValue = useVModel(props, 'modelValue', emits, { passive: true })
 </script>
 
 <template>
   <FieldWrapper :id="id" :label="label" :required="required" :errors="errors">
-    <Input :id="id" v-model="modelValue" :type="type" :placeholder="placeholder" :disabled="disabled" />
+    <Input :id="id" v-model="modelValue" :type="type" :placeholder="placeholder" :disabled="disabled" v-bind="$attrs" />
   </FieldWrapper>
 </template>
